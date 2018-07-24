288cc878ec77471fa6d4ede3ef2b7868e00f8cb4
freelyfranciz <36513809+freelyfranciz@users.noreply.github.com>
2018-07-24 18:03:05 -0500

    hibernate one-to-one
    One to one unidirectional and bidirectional

---
+++ hb-01-one-to-one-bi/src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,96 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Instructor_detail")
+public class InstructorDetail {
+	
+	//annotate the class as entity and map to db table
+	
+	//define the fields
+	
+	//annotate the fields with db column names
+	
+	//create constructors
+	
+	//generate setters and getters methods
+	
+	//generate toString() method
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String yotubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	//add new field for instructor also setter and getter
+	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	private Instructor instructor;
+	
+	
+	public InstructorDetail() {
+		
+	}
+	
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+
+	public InstructorDetail(String yotubeChannel, String hobby) {
+		this.yotubeChannel = yotubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYotubeChannel() {
+		return yotubeChannel;
+	}
+
+	public void setYotubeChannel(String yotubeChannel) {
+		this.yotubeChannel = yotubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", yotubeChannel=" + yotubeChannel + ", hobby=" + hobby + "]";
+	}
+	
+	
+	
+}
